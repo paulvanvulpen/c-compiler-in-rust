9108db45be7250caad58bb5d663de01fc6a9c1b4
Paul van Vulpen <paul@paulvanvulpen.nl>
2025-06-23 21:33:24 +0200

    Split update label map and resolve label name into two passes to cover more edge cases

--- src/compiler_driver/compiler/semantic_analysis/label_resolution.rs
+++ src/compiler_driver/compiler/semantic_analysis/label_resolution.rs
@@ -2,38 +2,55 @@ use super::generator;
 use super::parser;
 use anyhow::{Context, anyhow};
 use std::collections::HashMap;
-use std::fmt;
 
-#[derive(Debug, Clone)]
-struct LabelNotFound;
-
-impl fmt::Display for LabelNotFound {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Label not found")
-    }
-}
-
-impl std::error::Error for LabelNotFound {}
-
-fn resolve_statement(
-    statement: parser::Statement,
+fn update_label_map(
+    statement: &parser::Statement,
     label_map: &mut HashMap<String, String>,
-) -> anyhow::Result<parser::Statement> {
+) -> anyhow::Result<()> {
     match statement {
         parser::Statement::Label(identifier) => {
-            if label_map.contains_key(&identifier) {
+            if label_map.contains_key(identifier) {
                 return Err(anyhow!("Duplicate label statement!"));
             }
             let unique_name = generator::make_temporary_from_identifier(&identifier);
-            label_map.insert(identifier.clone(), unique_name.clone());
-            Ok(parser::Statement::Label(label_map[&identifier].clone()))
+            label_map.insert(identifier.clone(), unique_name);
+            Ok(())
         }
+        parser::Statement::If {
+            then_statement,
+            optional_else_statement,
+            ..
+        } => {
+            update_label_map(&*then_statement, label_map)?;
+            if let Some(else_statement) = optional_else_statement {
+                update_label_map(&*else_statement, label_map)?;
+            }
+            Ok(())
+        }
+        parser::Statement::Expression(..)
+        | parser::Statement::Return(..)
+        | parser::Statement::Goto(..)
+        | parser::Statement::Null => Ok(()),
+    }
+}
+
+fn resolve_statement(
+    statement: parser::Statement,
+    label_map: &HashMap<String, String>,
+) -> anyhow::Result<parser::Statement> {
+    match statement {
         parser::Statement::Goto(identifier) => {
             if label_map.contains_key(&identifier) {
                 Ok(parser::Statement::Goto(label_map[&identifier].clone()))
             } else {
-                Err(anyhow::Error::new(LabelNotFound))
+                Err(anyhow!(
+                    "goto statement to undeclared label {:?}",
+                    identifier
+                ))
             }
+        }
+        parser::Statement::Label(identifier) => {
+            Ok(parser::Statement::Label(label_map[&identifier].clone()))
         }
         parser::Statement::If {
             condition,
@@ -54,34 +71,29 @@ fn resolve_statement(
     }
 }
 
+// TODO: "when adding scopes, reevaluate this,
+//  inside nested ifs the condition that labels must be followed by a valid statement is not implicitly met"
 pub fn analyse(
     mut function_body: Vec<parser::BlockItem>,
 ) -> anyhow::Result<Vec<parser::BlockItem>> {
     let mut label_map: HashMap<String, String> = HashMap::new();
-    let mut goto_requests: Vec<&mut parser::BlockItem> = Vec::new();
+
+    for block_item in &function_body {
+        if let parser::BlockItem::Statement(statement) = block_item {
+            update_label_map(statement, &mut label_map)?;
+        }
+    }
 
     let mut is_last_item_label_statement = false;
     for block_item in &mut function_body {
         match block_item {
             parser::BlockItem::Statement(statement) => {
                 let original = std::mem::take(statement);
-                let resolved_statement = resolve_statement(original, &mut label_map)
-                    .context("analysed statement block item");
-                match resolved_statement {
-                    Ok(resolved_statement) => {
-                        is_last_item_label_statement =
-                            matches!(resolved_statement, parser::Statement::Label(..));
-                        *block_item = parser::BlockItem::Statement(resolved_statement);
-                    }
-                    Err(error) => {
-                        if error.downcast_ref::<LabelNotFound>().is_some() {
-                            goto_requests.push(block_item);
-                            is_last_item_label_statement = false;
-                        } else {
-                            return Err(error);
-                        }
-                    }
-                };
+                let resolved_statement = resolve_statement(original, &label_map)
+                    .context("analysed statement block item")?;
+                is_last_item_label_statement =
+                    matches!(resolved_statement, parser::Statement::Label(..));
+                *block_item = parser::BlockItem::Statement(resolved_statement);
             }
             parser::BlockItem::Declaration(..) => {
                 if is_last_item_label_statement {
@@ -97,18 +109,6 @@ pub fn analyse(
         return Err(anyhow!(
             "In C17, labels must be followed by a valid statement"
         ));
-    }
-
-    for request in goto_requests {
-        if let parser::BlockItem::Statement(parser::Statement::Goto(identifier)) = request {
-            if label_map.contains_key(identifier) {
-                *request = parser::BlockItem::Statement(parser::Statement::Goto(
-                    label_map[identifier].clone(),
-                ));
-            } else {
-                return Err(anyhow!("undeclared label {:?}", identifier));
-            }
-        }
     }
 
     Ok(function_body)
